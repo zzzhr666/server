745c447c10aa284a326b68eaae0bcee8fd27d200
zhanghaoran <haoranzhang1999@gmail.com>
2026-07-23 22:40:22 +0800

    [feature] Start battle runtime instances

--- battle-server/runtime/battle_runtime.cpp
+++ battle-server/runtime/battle_runtime.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstdint>
 
+#include "battle_instance.hpp"
 #include "game/game_manager.hpp"
 #include "net/packet_codec.hpp"
 #include "session/battle_session.hpp"
@@ -13,31 +14,39 @@ battle::BattleRuntime::BattleRuntime(RoomManager& room_manager, SessionManager& 
                                      SendPacketCallback callback)
     : room_manager_(room_manager), session_manager_(session_manager), send_packet_(std::move(callback)) {}
 
+battle::BattleRuntime::~BattleRuntime() = default;
+
 void battle::BattleRuntime::start_room(const std::string& room_name) {
     {
         std::lock_guard<std::mutex> lock(mutex_);
-        if (!started_rooms_.insert(room_name).second) {
+        if (starting_rooms_.contains(room_name) || instances_.contains(room_name)) {
             return;
         }
+        starting_rooms_.insert(room_name);
     }
+
     auto sessions = session_manager_.sessions_in_room(room_name);
+
     std::vector<std::int64_t> player_ids;
+    player_ids.reserve(sessions.size());
     for (const auto& session : sessions) {
         player_ids.push_back(session->player_id());
     }
+
+    auto instance = std::make_unique<BattleInstance>(BattleInstanceConfig{
+        .room_name = room_name,
+        .player_ids = player_ids,
+    });
+
     auto game_start_packet = make_game_start(room_name, player_ids);
+
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        starting_rooms_.erase(room_name);
+        instances_.emplace(room_name, std::move(instance));
+    }
+
     for (const auto& session : sessions) {
         send_packet_(game_start_packet, session->endpoint());
     }
-
-    //todo: in-game
-
-    auto game_over_packet = make_game_over(room_name, player_ids, "demo_complete");
-    for (const auto& session : sessions) {
-        send_packet_(game_over_packet, session->endpoint());
-    }
-    session_manager_.remove_room(room_name);
-    room_manager_.close_room(room_name);
-    std::lock_guard<std::mutex> lock(mutex_);
-    started_rooms_.erase(room_name);
 }
--- battle-server/runtime/battle_runtime.hpp
+++ battle-server/runtime/battle_runtime.hpp
@@ -2,9 +2,11 @@
 
 
 #include <functional>
-#include <unordered_set>
 #include <mutex>
 #include <string>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
 
 
 #include "net/udp_endpoint.hpp"
@@ -13,12 +15,14 @@
 namespace battle {
     class SessionManager;
     class RoomManager;
+    class BattleInstance;
 
     using SendPacketCallback = std::function<void(const v1::ServerPacket&, const UdpEndpoint&)>;
 
     class BattleRuntime {
     public:
-        BattleRuntime(RoomManager& room_manager,SessionManager& session_manager, SendPacketCallback callback);
+        BattleRuntime(RoomManager& room_manager, SessionManager& session_manager, SendPacketCallback callback);
+        ~BattleRuntime();
 
         void start_room(const std::string& room_name);
 
@@ -27,6 +31,7 @@ namespace battle {
         SessionManager& session_manager_;
         SendPacketCallback send_packet_;
         std::mutex mutex_;
-        std::unordered_set<std::string> started_rooms_;
+        std::unordered_map<std::string, std::unique_ptr<BattleInstance>> instances_;
+        std::unordered_set<std::string> starting_rooms_;
     };
 }
